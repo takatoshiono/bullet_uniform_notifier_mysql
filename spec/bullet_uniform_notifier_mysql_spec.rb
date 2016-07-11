7e9708ad355344463f5977c5bc8f20da5e5a2f0d
Takatoshi Ono <takatoshi.ono@gmail.com>
2016-07-11 18:52:17 +0900

    Delete a test

--- spec/bullet_uniform_notifier_mysql_spec.rb
+++ spec/bullet_uniform_notifier_mysql_spec.rb
@@ -4,8 +4,4 @@ describe BulletUniformNotifierMysql do
   it 'has a version number' do
     expect(BulletUniformNotifierMysql::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    expect(false).to eq(true)
-  end
 end
